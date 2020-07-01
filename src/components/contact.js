c9e9c11d817432140f3a659da847b96001121c7e
andrewpblee <andrewpblee@gmail.com>
2020-07-01 20:28:57 +0100

    opening links in new tab

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,11 +4,11 @@ const Contact = () => {
   return (
     <div className="contact">
       <p className="contactLink">
-        <a href="https://github.com/andrewpblee">Github</a>
+        <a href="https://github.com/andrewpblee" target="_blank">Github</a>
       </p>
       <p className="contactLink"> |</p>
       <p className="contactLink">
-        <a href="https://www.linkedin.com/in/andrewpblee/">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/andrewpblee/" target="_blank">LinkedIn</a>
       </p>
       <p className="contactLink">|</p>
       <p className="contactLink">
--- src/components/project.js
+++ src/components/project.js
@@ -4,7 +4,7 @@ const Project = props => {
   return (
     <div className="project">
       <h2 className="projectName">
-        <a href={props.src}>{props.projectName}</a>
+        <a href={props.src} target="_blank">{props.projectName} </a>
       </h2>
       <p className="bio">{props.bio}</p>
       <p className="languages">{props.language}</p>
