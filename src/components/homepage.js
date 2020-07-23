2fd158d3b8686f00a27cfa73c6829fc0c4acca20
andrewpblee <andrewpblee@gmail.com>
2020-07-23 16:40:52 +0100

    fixing buttons

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -4,25 +4,27 @@ import Icon from "../components/IconFooterHP"
 
 const Homepage = props => {
   return (
-    <div className="hp-wrapper">
-      <div className={`hi-im-andrew ${props.togglestate}`}>
-        <h1>
-          Hi<span>,</span>
-        </h1>
-        <h2>I'm Andrew</h2>
+    <>
+      <div className="hp-wrapper">
+        <div className={`hi-im-andrew ${props.togglestate}`}>
+          <h1>
+            Hi<span>,</span>
+          </h1>
+          <h2>I'm Andrew</h2>
+        </div>
+        <p className={`text ${props.togglestate}`}>
+          I'm an analyst looking to move into engineering. Here are some
+          projects I've been working on.
+        </p>
+        <Link
+          to="/portfolio/"
+          style={{ textDecoration: "none", color: "#2b2d42" }}
+        >
+          <button className={`button-${props.togglestate}`}>Take a look</button>
+        </Link>
       </div>
-      <p className={`text ${props.togglestate}`}>
-        I'm an analyst looking to move into engineering. Here are some projects
-        I've been working on.
-      </p>
-      <Link
-        to="/portfolio/"
-        style={{ textDecoration: "none", color: "#2b2d42" }}
-      >
-        <button className={`button-${props.togglestate}`}>Take a look</button>
-      </Link>
       <Icon repo={"https://github.com/andrewpblee"} />
-    </div>
+    </>
   )
 }
 export default Homepage
